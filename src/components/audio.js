4da14ce0d054925369f50216fbccb6a0acd1e8e4
shettypuneeth <puneeth102@gmail.com>
2015-12-18 20:04:04 +0530

    code refactor: Alignment

--- src/components/audio.js
+++ src/components/audio.js
@@ -4,25 +4,25 @@ import shallowEqual from '../utils/shallowEqual';
 
 export default class AudioElement extends React.Component {
 
-	shouldComponentUpdate (nextProps, nexState) {
-		return !shallowEqual(this.props.sources, nextProps.sources);
-	}
+  shouldComponentUpdate (nextProps, nexState) {
+    return !shallowEqual(this.props.sources, nextProps.sources);
+  }
 
-	componentDidUpdate () {
-		this.props.loadSource();
-	}
+  componentDidUpdate () {
+    this.props.loadSource();
+  }
 
-	render () {
-		let sources = this.props.sources; 
-		return (
-			<div>
-				<audio>
-				{
-					sources.map(s=> <source key={s.src} src={s.src} type={s.type}/>)
-				}
-				</audio>
-			</div>
-		);
-	}
+  render () {
+    let sources = this.props.sources; 
+    return (
+      <div>
+	    <audio>
+	      {
+		    sources.map(s=> <source key={s.src} src={s.src} type={s.type}/>)
+	      }
+	    </audio>
+      </div>
+    );
+  }
 }
 
